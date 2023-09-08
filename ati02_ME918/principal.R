5ac797e5342a91880e0828ba7f295f0752f619d9
ti-cruz <102383492+ti-cruz@users.noreply.github.com>
2023-09-08 11:50:43 -0300

    Correção da atividade/laboratório 2
    Obs.: Resta tirar algumas duvidas dessa atividade.

--- ati02_ME918/principal.R
+++ ati02_ME918/principal.R
@@ -2,6 +2,8 @@ library(jsonlite)
 library(tidyverse)
 library(yaml)
 
+## Gabarito = Realizado em sala de aula.
+
 # Parte 0
 
 fromJSON(paste0("dados/relatorio1.json"), 
@@ -10,12 +12,25 @@ fromJSON(paste0("dados/relatorio1.json"),
 # Parte 2
 
 rl <- read_yaml("configuracoes.yaml")
-rl2 <- rl[[1]]
+
+lidos <- purrr::map(rl$arquivos, function(x) {
+  read_json(file.path("dados", x))
+})
 
 rlt <- list()
 for (i in 1:length(rl$arquivos)) { 
-  rlt <- append(rlt, read_json(paste0("dados/",rl2[i])))
+  rlt <- append(rlt, lidos[[i]])
 }
+
+## Gabarito 
+
+### configuracoes <- yaml::read_yaml("configuracoes.yaml")
+
+### purrr::map(configuracoes$arquivos, function(x) {
+###   read_json(file.path("dados", x))
+### }) -> lidos
+
+### listao <- do.call(c, args = lidos)
 
 # Parte 3
 
@@ -25,7 +40,25 @@ obj <- map(rlt, data.frame)
 df <- list_rbind(obj)
 
 df <- df %>%
-  mutate(horario = as.POSIXct(horario)) 
+  mutate(horario = as.POSIXct(horario)) %>%
+  as_tibble()
+
+## Gabarito
+
+### library(purrr)
+
+### map(listao, as.data.frame) -> listao_df
+### list_rbind(listao_df) -> tabelao
+
+### library(tidyverse)
+### tabelao <- tibble(tabelao)
+### tabelao$horario <-  lubridate::as_datetime(tabelao$horario)
+
+### tabelao %>%
+###  filter(evento == "recalibragem") %>%
+###  select(horario, evento) %>%
+###  arrange(horario) %>%
+###  mutate(total = 1:n()) -> tabela_r
 
 # Parte 4
 
@@ -33,37 +66,54 @@ library(ggplot2)
 
 df %>%
   filter(evento %in% "recalibragem") %>%
+  select(horario, evento) %>%
+  arrange(horario) %>%
+  mutate(total = 1:n()) %>%
+  ggplot(aes(x = horario, y = total)) +
+  geom_line() +
+  labs(title = "Quantidade de recalibragens acumuladas 
+  a cada instante de tempo",
+       x = "Horário",
+       y = "Total")+
+  theme_bw()
+
+## Resposta: De acordo com o gráfico, podemos reparar que existe dois 
+## periodos que foram causado dano no sistema de recalibagem. 
+
+horario_vali <- df %>%
+  filter(evento == "recalibragem") %>%
   select(horario) %>%
-  mutate(`Horário acumulado` =  as.integer(format(horario, "%H")),
-         `Dia da Semana` = str_to_title(format(horario, "%A"))) %>%
-  ggplot(aes(x = `Dia da Semana`, y = `Horário acumulado`)) +
-  geom_point(alpha = 0.16, size = 4)+
-  labs(title = "Dispersão dos horários acumulados de recalibragem 
-no período dos registros.")+
-  scale_x_discrete(labels = c("Domingo", "Segunda-Feira", 
-                              "Terça-Feira", "Quarta-Feira", 
-                              "Quinta-Feira", "Sexta-Feira",
-                              "Sábado"))+
-  theme_minimal()
+  mutate(anterior = lag(horario)) %>%
+  mutate(diferenca = horario - anterior) %>%
+  filter(diferenca > 8*60*60)
 
-## Resposta: De acordo com o gráfico, quanto mais a coloração das observações 
-## são escuras, mais a quantidade de registro foi obtida no intervalo do tempo
-## no dia da semana. Dessa maneira, vale relatar que existe dados ausente 
-## no horario das 5:00:00 às 5:59:59 na quinta-feira.
+df_gg <- df %>%
+  filter(horario < horario_vali$anterior[1] |
+           horario > horario_vali$horario[1]) %>%
+  filter(horario < horario_vali$anterior[2] |
+           horario > horario_vali$horario[2])
 
-l = 1
-df_rem_8 <- df %>% arrange(horario)
-for (i in 1:nrow(df)) {
-  
-  if (df[i,]$evento %in% "recalibragem") {
-    
-    if (abs(df[i,]$horario - df[l,]$horario) > 8) {
-      df_rem_8  <- df_rem_8[-c(l:i-1),]
-    }
-    
-    l=i+1
-    }
-}
+## Duvida: Como transformaria os "filter()" em laço "for(){}" ?
+## pois, se tiver "n" intevalos de 8 horas, fica impossibilitado de
+## fazer a quantidade de "filter()" na mão.
+
+## Gabarito
+
+### ggplot(tabela_r, aes(x = horario, y = total)) +
+### geom_line()
+
+### tabelao %>%
+###   filter(evento == "recalibragem") %>%
+###   select(horario) %>%
+###   mutate(anterior = lag(horario)) %>%
+###   mutate(diferenca = horario - anterior) %>%
+###   filter(diferenca > 8*60*60) -> horario_validos
+
+### tabelao %>%
+###   filter(horario < horario_validos$anterior[1] |
+###            horario > horario_validos$horario[1]) %>%
+###   filter(horario < horario_validos$anterior[2] |
+###            horario > horario_validos$horario[2]) -> tabelao_bom
 
 # Parte 5
 
@@ -76,8 +126,7 @@ lms_intensidade <- function(Nomes_variaveis){
   list_lms <- list()
   
   for (i in 1:length(Nomes_variaveis)) {
-    df_lms <- df_rem_8 %>%
-      filter(evento %in% "medida") %>% 
+    df_lms <- df_gg %>%
       select(intensidade,{{Nomes_variaveis}}[[i]])
     
     list_lms <- append(list_lms, list(lm(intensidade~., data = df_lms)))
@@ -85,5 +134,18 @@ lms_intensidade <- function(Nomes_variaveis){
   return(list_lms)
 }
 
-lms_intensidade(Nomes_comb)
+## lms_intensidade(Nomes_comb)
+
+## Gabarito
+
+### combin <- expand.grid(v1 = c(T,F), v2 = c(T,F), v3 = c(T,F), v4 = c(T,F))
+### l <- unlist(apply(combin, 1, list), recursive = FALSE)
+### nomes <- lapply(l, function(x) names(x)[x])
+
+### f <- function(n){
+###   aux <- tabelao_bom[, c("intensidade", n)]
+  ###   lm(intensidade ~ ., data = aux)
+### }
+
+### map(nomes, f)
   
